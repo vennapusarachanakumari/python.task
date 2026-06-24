b0a2d2835693790faea15188aedca78a3c8ca8de
vennapusarachanakumari <vennapusarachana2@gmail.com>
2026-06-24 14:15:49 +0530

    Add files via upload

---
+++ bitwise.py
@@ -0,0 +1,37 @@
+#Perform XOR if two values are same what will be the o\p
+a=6
+b=6
+print(a^b)
+
+#Perform AND operator 1 and even
+a=1
+b=8
+print(a&b)
+
+#Perform AND operator 1 and odd
+a=1
+b=9
+print(a&b)
+
+
+#All bitwise operators
+a=int(input("enter first value="))
+b=int(input("enter second value="))
+print(a&b)
+print(a|b)
+print(a^b)
+print(~a)
+print(a>>b)
+print(a<<b)
+a&=b
+print(a)
+a|=b
+print(a)
+a^=b
+print(a)
+~a
+print(a)
+a<<=b
+print(a)
+a>>=b
+print
