b4234e7369a54b0f147f8fcfe8fa4659e82f4b25
vennapusarachanakumari <vennapusarachana2@gmail.com>
2026-06-25 15:24:28 +0530

    Add files via upload

---
+++ errors.py
@@ -0,0 +1,21 @@
+#type error
+age=input("enter the age=")
+print(age+5)
+
+#value error
+num=int(input("enter the number="))
+print(num)
+
+#Name error
+print(name)
+
+#syntax error
+print("hello world"
+      
+name=input("enter the name=")
+age=int(input("enter the age="))
+print(name,age,sep=",")
+
+a=input("enter the value=")
+b=input("enter the value=")
+print(a+b)
