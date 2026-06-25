1ed1f503da83b0e19c1eacac2e19ecd40d264ad2
vennapusarachanakumari <vennapusarachana2@gmail.com>
2026-06-25 19:47:28 +0530

    Add files via upload

---
+++ slicing.py
@@ -0,0 +1,38 @@
+s = "PYTHONPROGRAM"
+#1. Print entire string
+print(s[:])
+
+
+#2. First 6 characters
+print(s[:6])
+
+
+#3. From index 6 to end
+print(s[6:])
+
+#4. Characters from index 2 to 7
+print(s[2:8])
+
+
+#5. Every second character
+print(s[::2])
+
+
+#6. Reverse the string
+print(s[::-1])
+
+
+#7. Last 5 characters
+print(s[-5:])
+
+
+#8. Remove first and last character
+print(s[1:-1])
+#9. Every third character
+print(s[::3])
+
+
+#10. Reverse every second character
+print(s[::-2])
+
+
