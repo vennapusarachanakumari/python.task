ac302d3327b322593fbb25011cd19e743104bb91
vennapusarachanakumari <vennapusarachana2@gmail.com>
2026-06-25 12:53:26 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,110 @@
+#arithmetic operator:
+# x=10
+# y=20
+# print(x+y)
+# print(x-y)
+# print(x*y)
+# print(x/y)
+# print(x//y)
+# print(x%y)
+# print(x**y)
+#assignment operator:
+# x=10
+# y=5
+# x+=y
+# print(x)
+# x-=y
+# print(x)
+# x*=y
+# print(x)
+# x/=y
+# print(x)
+# x//=y
+# print(x)
+# x%=y
+# print(x)
+# x**=y
+# print(x)
+#relational operator:
+# x=10
+# y=5
+# print(x<y)
+# print(x>y)
+# print(x<=y)
+# print(x>=y)
+# print(x==y)
+# print(x!=y)
+#bitwise operator:
+# a=int(input("enter first value="))
+# b=int(input("enter second value="))
+# print(a&b)
+# print(a|b)
+# print(a^b)
+# print(~a)
+# print(a>>b)
+# print(a<<b)
+
+# #strings
+
+# s1="panda"
+# s2="bala"
+# s3="siri"
+# print(s1>s2)
+# print(s3<s2)
+# True
+# False
+#multiple values:
+
+# x=20
+# y=10
+# z=30
+# print(x>y<z)
+# print(x<y>z)
+# True
+# False
+# #logical operators:
+
+# x=12
+# y=30
+# z=10
+# print((x<y) and (y>z))
+# print((x<y) and (x>z) and (z>y))
+# print((x<z) or (z>y))
+# print((x<y) or (x>y) or (z>y))
+# print((not(x<z)))
+
+# #and or xor
+# a=10
+# b=8
+# print(a&b)
+# print(a|b)
+# print(a^b)
+# n=32
+# print(n&(n-1)==0)
+#swapping two numbers without using 3rd var and a,b=b,a using bits
+# a=9
+# b=7
+# print("before swapping",'a=',a,'b=',b)
+# a^=b
+# b^=a
+# a^=b
+# print("after swapping",'a=',a,'b=',b)
+
+#Membership operator
+
+#Not 
+# -(a+b) positive
+a=10
+# print(~a)
+# # +(a-b) Negitive
+# a=-10
+# print(~a)
+# -11
+# 9
+# #Lift shift
+# a=10
+# print(a<<2)
+# #Right shift
+# a=10
+# print(a>>5)
+ 
