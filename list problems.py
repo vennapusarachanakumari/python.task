abd27db8f42aafbd0f41a7f388ca2be0f4d2e181
vennapusarachanakumari <vennapusarachana2@gmail.com>
2026-06-26 10:41:47 +0530

    Add files via upload

--- list problems.py
+++ list problems.py
@@ -1,37 +1,37 @@
-#sum of list
+# #sum of list
 
-l=[10,20,60,30,40]
-sum=0
-for val in l:
-    sum+=val
-print(sum)
+# l=[10,20,60,30,40]
+# sum=0
+# for val in l:
+#     sum+=val
+# print(sum)
 
-#product of list
+# #product of list
 
-l=[10,20,60,30,40]
-prod=1
-for val in l:
-    prod=prod*val
-print(prod)
+# l=[10,20,60,30,40]
+# prod=1
+# for val in l:
+#     prod=prod*val
+# print(prod)
 
-#maximum list
-l=[10,20,60,30,40]
-max=0
-for val in l:
-    if max<val:
-      max=val
-print(max)
+# #maximum list
+# l=[10,20,60,30,40]
+# max=0
+# for val in l:
+#     if max<val:
+#       max=val
+# print(max)
 
-#minimum list
+# #minimum list
 
-l=[10,20,60,30,40]
-min=l[0]
-for val in l:
-    if min>val:
-      min=val
-print(min)
+# l=[10,20,60,30,40]
+# min=l[0]
+# for val in l:
+#     if min>val:
+#       min=val
+# print(min)
 
-#length of list
+# #length of list
 
-l = [10, 20, 60, 30, 40]
-print(len(l))
+# l = [10, 20, 60, 30, 40]
+# print(len(l))
